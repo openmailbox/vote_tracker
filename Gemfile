4f569c18b8bdef334a488a8004b77d6b8cb64a24
Brandon Rice <brice84@gmail.com>
2017-03-08 14:32:00 -0500

    Update Gemfile for deploy.

--- Gemfile
+++ Gemfile
@@ -7,4 +7,5 @@ gem 'whenever', require: false
 group :development do
   gem 'capistrano', '~> 3.4.0'
   gem 'capistrano-bundler', '~> 1.1.2'
+  gem 'capistrano-rvm'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
+set :rvm_ruby_version, '2.3.3@vote_tracker'
+
 set :application, 'congress_tracker'
 set :repo_url, 'https://github.com/nevern02/vote_tracker.git'
 
